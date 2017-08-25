8ae9c859a133f47b166d34d077273d0018013371
Holden Karau <holden@us.ibm.com>
2017-08-25 10:02:48 -0700

    Force coverage >=4.2.0 also cat the coveragerc file since the travis error is weird

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     install_requires=INSTALL_REQS,
     tests_requires=[
         'nose==1.3.7',
-        'coverage>4.0.0',
+        'coverage>4.2.0',
         'unittest2>=1.0.0',
         'testinstances',
     ],
